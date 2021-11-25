44cee84bd8f600e16d3b76dbff9147a64bf74930
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-25 16:02:42 -0600

    Creando la estructura

--- App.js
+++ App.js
@@ -5,13 +5,27 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 export default function App(){
 
   return (
-    <SafeAreaView>
-      <Text>Maquino</Text>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.safeArea}>
+        <Text>Formulario header</Text>
+      </SafeAreaView>
+      <View>
+        <Text>Resultado</Text>
+      </View>
+      <View>
+        <Text>Footer</Text>
+      </View>
+    </>
   )
 }
 
 
 const styles= StyleSheet.create({
-  
+  safeArea: {
+    backgroundColor: '#f00',
+    height: 200,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    alignItems: 'center',
+  }
 });
