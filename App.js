ae43e3974b0c7edc6b7b88261541f0b97ad01c20
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-25 15:45:00 -0600

    Creacion del proyecto

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+
+
+export default function App(){
+
+  return (
+    <SafeAreaView>
+      <Text>Maquino</Text>
+    </SafeAreaView>
+  )
+}
+
+
+const styles= StyleSheet.create({
+  
+});
